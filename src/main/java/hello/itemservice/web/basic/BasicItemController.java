97fc6e54d4aa7e7ff925c4cc87dc5839d264d729
DESKTOP-0V6S1TK\JIN_HANSUNG_2 <jyj1641@gmail.com>
2022-09-06 00:08:13 +0900

    스프링 MVC - 웹페이지 만들기

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,63 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String add(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@ModelAttribute("item") Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/"+item.getId();
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String itemEdit(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/update")
+    public String itemSave(@RequestParam("id") Long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items";
+    }
+
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
